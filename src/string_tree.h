207804aab231b75bb82f7f1c1b87de7b94bf3235
Assemberist <schur.alexandr2015@yandex.ru>
2023-06-22 13:46:16 +0300

    Created prototypes of desired functions.

--- src/string_tree.h
+++ src/string_tree.h
@@ -13,7 +13,19 @@ typedef struct token{
 
 token* init_tree();
 void add_element(char* src, void* element, token* begin);
+void update_element(char* src, void* element, token* begin);
 void* find_element(char* src, token* begin);
 void remove_tree(token* begin);
+
+void* pack_tree(token* begin);
+token* unpack_tree(void* pack);
+
+void import_words(char* path);
+void import_tree(char* path);
+
+void export_tree(char* path, void* pack);
+
+void* update_pack(char* src, void* element, void* begin);
+void* find_element(char* src, token* begin);
 
 #endif
