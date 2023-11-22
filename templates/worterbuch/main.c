57041615c99801309c7cf527a2e1cd293f64a6df
Assemberist <schur.alexandr2015@yandex.ru>
2023-11-22 15:55:23 +0000

    Template infrastructure added.

---
+++ templates/worterbuch/main.c
@@ -0,0 +1,19 @@
+#include "string_tree.h"
+#include <stdio.h>
+
+int main(){
+    token* begin = init_tree();					// string_tree is created. All memory is dynamically allocated.
+
+    add_element("arka", (void*)1, begin);
+    add_element("arfa", (void*)2, begin);
+    add_element("ar", (void*)3, begin);
+
+	printf("\"arka\" found -> %i\n",
+			find_element("arka", begin));
+	printf("\"arfa\" found -> %i\n",
+			find_element("arka", begin)); 
+	printf("\"none\" not found -> %i\n", 
+			find_element("none", begin));
+
+	remove_tree(begin);							// frees used memory.
+}
